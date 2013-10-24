114e44862c3b1716ede6f5b23a8793603ef495ca
sidharrell <sidney.harrell@gmail.com>
2013-10-24 13:24:38 -0400

    Create CustomFunctionsPlugin.php

---
+++ CustomFunctionsPlugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Custom Functions Plugin
+ * Plugin URI: http://eventespresso.com
+ * Description: A blank plugin where you can put custom functions
+ * Version: 1.0
+ * Author: Sidney Harrell
+ * Author URI: http://sidneyharrell.com
+ * License: GPL2
+ */
+ 
+ 
