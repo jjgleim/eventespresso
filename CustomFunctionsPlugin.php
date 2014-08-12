83edd9eda180022d22ebe926427355ddcdf415a1
Sidney Harrell <sidney.harrell@gmail.com>
2014-08-12 16:25:53 -0400

    Plugin name was conflicting with a plugin on wp.org

--- CustomFunctionsPlugin.php
+++ CustomFunctionsPlugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Custom Functions Plugin
+ * Plugin Name: Blank Plugin
  * Plugin URI: http://eventespresso.com
  * Description: A blank plugin where you can put custom functions
  * Version: 1.0
